7c1b4533b6dc0e4cc67e4a98cc687151f393513c
Peiman_Django <djangobypeiman@gmail.com>
2023-06-12 13:44:30 +0330

    create signup and change fields form and Complete login

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,14 +4,15 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ('age',)
+
+        fields = ('username', 'age', 'email',)
 
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = CustomUser
-        fields = UserChangeForm.Meta.fields
+        fields = ('username', 'age', 'email',)
 
 
 
--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
+from . import views
 urlpatterns = [
-    # path('signup/', views.SignUpView.as_view(), name='signup'),
+    path('signup/', views.SignUpView.as_view(), name='signup'),
 
 ]
 
