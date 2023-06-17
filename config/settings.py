e917e7185089ea186c8f1083e8bdb548ba495774
Peiman_Django <djangobypeiman@gmail.com>
2023-06-17 08:16:07 +0330

    create templates password and modified settings , signup.html , login.html

--- config/settings.py
+++ config/settings.py
@@ -142,3 +142,23 @@ LOGOUT_REDIRECT_URL = 'home'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+
+#Email Config
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
