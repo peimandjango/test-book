7a938242018f366b2cd5b6b595aa4c03a2b35587
Peiman_Django <djangobypeiman@gmail.com>
2023-06-29 12:17:31 +0330

    view books urls config urls

--- books/views.py
+++ books/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
 
+from django.views import generic
+
+from .models import BOOK
 # Create your views here.
+
+class BookListView(generic.ListView):
+    model = BOOK
+    template_name = 'books/book_list'
+    context_object_name = 'books'
